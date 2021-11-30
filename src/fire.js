82365b3d14c348891708016ffc1c64ccd1b0dd09
Gagan Bhat <gaganbhat6@gmail.com>
2021-11-29 19:18:24 -0600

    Remove sensitive info and revoke keys

--- src/fire.js
+++ src/fire.js
@@ -2,12 +2,6 @@ import firebase from "firebase";
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBiOB1qVcGQ4ToS54xBHpi-yy3kuPUsNws",
-    authDomain: "login-3e284.firebaseapp.com",
-    projectId: "login-3e284",
-    storageBucket: "login-3e284.appspot.com",
-    messagingSenderId: "963865090247",
-    appId: "1:963865090247:web:55aff670876094a465c6e2"
   };
 
 const fire = firebase.initializeApp(firebaseConfig);
